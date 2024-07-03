9fa40259d9b4311b4ff5b6fd68e8ef90b0d62a09
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-07-03 23:53:43 +0530

    added query finding

--- JAVA_17_Array_4.java
+++ JAVA_17_Array_4.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class ArrayFour {
     static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -38,6 +40,31 @@ class ArrayFour {
 //        reverse 0 to n-1
         reverseInArray(arr, 0, n - 1);
     }
+
+    static int[] makeFrequencyArray(int[] arr) {
+        int[] freq = new int[100005];
+        for (int i = 0; i < arr.length; i++) {
+            freq[arr[i]]++;
+        }
+
+        return  freq;
+    }
+
+    static void queryFind(int[] arr) {
+        int[] freqArray = makeFrequencyArray(arr);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of query you want to run");
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            System.out.println("Enter number");
+            int num = sc.nextInt();
+            if(freqArray[num] > 0) {
+                System.out.println("Present");
+            }else {
+                System.out.println("Not present");
+            }
+        }
+    }
 }
 
 public class JAVA_17_Array_4 {
@@ -48,9 +75,15 @@ public class JAVA_17_Array_4 {
 //        ArrayFour.reverseArray(arr_1);
 //        ArrayFour.printArray(arr_1);
 
+
+//        rotate an array in place
         int[] arr_2 = {2, 4, 5, 6, 7};
-        ArrayFour.printArray(arr_2);
-        ArrayFour.rotateArrayInPlace(arr_2, 2);
-        ArrayFour.printArray(arr_2);
+//        ArrayFour.printArray(arr_2);
+//        ArrayFour.rotateArrayInPlace(arr_2, 2);
+//        ArrayFour.printArray(arr_2);
+
+//        query finding - frequency array
+        int[] arr_3 = {2, 4, 56, 76, 3};
+        ArrayFour.queryFind(arr_3);
     }
 }
