05abba961f7948876ba5821eb9bffd44f911ac0f
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-07-03 23:20:26 +0530

    rotate an array by k steps

--- JAVA_17_Array_4.java
+++ JAVA_17_Array_4.java
@@ -17,14 +17,40 @@ class ArrayFour {
             j--;
         }
     }
+
+    static void reverseInArray(int[] arr, int i, int j) {
+        while(i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    static void rotateArrayInPlace(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+//        reverse 0 to n-k-1
+        reverseInArray(arr, 0 , n - k -1);
+//        reverse n-k to n-1
+        reverseInArray(arr, n - k, n - 1);
+//        reverse 0 to n-1
+        reverseInArray(arr, 0, n - 1);
+    }
 }
 
 public class JAVA_17_Array_4 {
     public static void main(String[] args) {
 //        reverse an array
         int[] arr_1 = {2, 5, 6, 2, 7};
-        ArrayFour.printArray(arr_1);
-        ArrayFour.reverseArray(arr_1);
-        ArrayFour.printArray(arr_1);
+//        ArrayFour.printArray(arr_1);
+//        ArrayFour.reverseArray(arr_1);
+//        ArrayFour.printArray(arr_1);
+
+        int[] arr_2 = {2, 4, 5, 6, 7};
+        ArrayFour.printArray(arr_2);
+        ArrayFour.rotateArrayInPlace(arr_2, 2);
+        ArrayFour.printArray(arr_2);
     }
 }
