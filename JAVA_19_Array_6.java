f0fa0d05d67a47965550da55f81b793dac1f2c4d
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-07-05 23:50:01 +0530

    equal sum partition

--- JAVA_19_Array_6.java
+++ JAVA_19_Array_6.java
@@ -14,6 +14,27 @@ class PrefixSum {
         }
         System.out.println();
     }
+
+    static int findTotalSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    static boolean equalSumPartition(int[] arr) {
+        int totalSum = findTotalSum(arr);
+        int prefixSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum += arr[i];
+            int suffixSum = totalSum - prefixSum;
+            if (prefixSum == suffixSum) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 public class JAVA_19_Array_6 {
@@ -27,21 +48,28 @@ public class JAVA_19_Array_6 {
 //        where l and r follows 1 based indexing
 //        first make prefix sum array
         int[] arr_2 = {2, 4, 5, 6, 2, 8, 3};
-        int[] prefixSumArray = PrefixSum.prefixSumArray(arr_2);
-        System.out.println("enter number of queries");
-        Scanner sc = new Scanner(System.in);
-        int q = sc.nextInt();
-        for (int i = 0; i < q; i++) {
-            System.out.println("Enter starting index");
-            int s = sc.nextInt();
-            System.out.println("Enter last index");
-            int l = sc.nextInt();
-            if (s == 1) {
-                System.out.println("Sum is " + prefixSumArray[l - 1]);
-            } else {
-                System.out.println("Sum is " + (prefixSumArray[l - 1] - prefixSumArray[s - 2]));
-            }
-        }
+//        int[] prefixSumArray = PrefixSum.prefixSumArray(arr_2);
+//        System.out.println("enter number of queries");
+//        Scanner sc = new Scanner(System.in);
+//        int q = sc.nextInt();
+//        for (int i = 0; i < q; i++) {
+//            System.out.println("Enter starting index");
+//            int s = sc.nextInt();
+//            System.out.println("Enter last index");
+//            int l = sc.nextInt();
+//            if (s == 1) {
+//                System.out.println("Sum is " + prefixSumArray[l - 1]);
+//            } else {
+//                System.out.println("Sum is " + (prefixSumArray[l - 1] - prefixSumArray[s - 2]));
+//            }
+//        }
 
+
+//        check if we can partition the array into two sub array, more formally check that prefix sum at any point is
+//        equal to the suffix sum
+        int[] arr_3 = {1, 2, 3, 4, 5};
+        int[] arr_4 = {1, 1, 1, 1, 1, 1};
+        System.out.println(PrefixSum.equalSumPartition(arr_3));
+        System.out.println(PrefixSum.equalSumPartition(arr_4));
     }
 }
