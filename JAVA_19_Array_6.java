6c5979e0af88b63d79b5b70b7e09275466efb720
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-07-05 23:27:33 +0530

    Range sum query using prefix sum

--- JAVA_19_Array_6.java
+++ JAVA_19_Array_6.java
@@ -1,5 +1,7 @@
-class PrefixSum{
-    static int [] prefixSumArray(int[] arr) {
+import java.util.Scanner;
+
+class PrefixSum {
+    static int[] prefixSumArray(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             arr[i] += arr[i - 1];
         }
@@ -23,6 +25,23 @@ public class JAVA_19_Array_6 {
 
 //        given array of integers, answer q queries where you have tell the sum of a given range from l to r(both include)
 //        where l and r follows 1 based indexing
+//        first make prefix sum array
+        int[] arr_2 = {2, 4, 5, 6, 2, 8, 3};
+        int[] prefixSumArray = PrefixSum.prefixSumArray(arr_2);
+        System.out.println("enter number of queries");
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        for (int i = 0; i < q; i++) {
+            System.out.println("Enter starting index");
+            int s = sc.nextInt();
+            System.out.println("Enter last index");
+            int l = sc.nextInt();
+            if (s == 1) {
+                System.out.println("Sum is " + prefixSumArray[l - 1]);
+            } else {
+                System.out.println("Sum is " + (prefixSumArray[l - 1] - prefixSumArray[s - 2]));
+            }
+        }
 
     }
 }
