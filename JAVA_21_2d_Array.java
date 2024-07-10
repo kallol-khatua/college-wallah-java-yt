53fd5ca5c7c3f9b6c6ce6cb3a69482f818ace533
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-07-10 11:47:04 +0530

    rotate square matrix by 90 degreee

--- JAVA_21_2d_Array.java
+++ JAVA_21_2d_Array.java
@@ -21,13 +21,34 @@ class TwoDimensionalArrayPractice {
     static void transposeInPlace(int[][] arr) {
 //        check for square matrix
 
-        for(int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr[0].length; j++) {
                 int temp = arr[i][j];
                 arr[i][j] = arr[j][i];
                 arr[j][i] = temp;
             }
         }
+    }
+
+    static void reverseRow(int[][] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            int j = 0, k = arr[i].length - 1;
+            while (j < k) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][k];
+                arr[i][k] = temp;
+                j++;
+                k--;
+            }
+        }
+    }
+
+    static void rotate90Degree(int[][] arr) {
+//        transpose
+        transposeInPlace(arr);
+
+//        reverse each row
+        reverseRow(arr);
     }
 
 }
@@ -47,9 +68,21 @@ public class JAVA_21_2d_Array {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        TwoDimensionalArrayPractice.print2dArray(arr_2);
-        TwoDimensionalArrayPractice.transposeInPlace(arr_2);
+//        TwoDimensionalArrayPractice.print2dArray(arr_2);
+//        TwoDimensionalArrayPractice.transposeInPlace(arr_2);
+//        System.out.println();
+//        TwoDimensionalArrayPractice.print2dArray(arr_2);
+
+
+//        rotate square matrix by 90 degree
+        int[][] arr_3 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        TwoDimensionalArrayPractice.print2dArray(arr_3);
+        TwoDimensionalArrayPractice.rotate90Degree(arr_3);
         System.out.println();
-        TwoDimensionalArrayPractice.print2dArray(arr_2);
+        TwoDimensionalArrayPractice.print2dArray(arr_3);
     }
 }
