666239afafc5b5331cd3c6e9e8fe897c48b104c4
Kallol Khatua <kallolkhatua2005@gmail.com>
2024-07-10 12:15:14 +0530

    impoved pascal's triangle

--- JAVA_21_2d_Array.java
+++ JAVA_21_2d_Array.java
@@ -78,12 +78,17 @@ class TwoDimensionalArrayPractice {
         int[][] ans = new int[n][];
         for (int i = 0; i < n; i++) {
             ans[i] = new int[i + 1];
-            for (int j = 0; j < i + 1; j++) {
-                if (j == 0 || j == i) {
-                    ans[i][j] = 1;
-                } else {
-                    ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
-                }
+//            for (int j = 0; j < i + 1; j++) {
+//                if (j == 0 || j == i) {
+//                    ans[i][j] = 1;
+//                } else {
+//                    ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
+//                }
+//            }
+
+            ans[i][0] = ans[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1];
             }
         }
         return ans;
